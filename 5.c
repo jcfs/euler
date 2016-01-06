b85a3f3e67ded4424029889a05f53453b9dc8829
jcfs <joao.salavisa@gmail.com>
2016-01-06 11:12:58 +0000

    prob 5

--- 5.c
+++ 5.c
@@ -2,24 +2,30 @@
 
 int main(int argc, char ** argv) {
 
-  int n = 2500;
+  int n = 2520;
 
   while(1) {
-    n++;
+    n+=2520;
 
     int i = 0;
     int f = 1;
 
-    for(i = 1; i <= 20; i++) {
+    for(i = 10; i <= 20; i++) {
       if (n % i) {
         f = 0;
       }
+    }
+
+    if (n % 2520) {
+      f = 0;
     }
 
     if (f) {
       printf("%d\n", n);
       break;
     }
+
+    printf("%d\n", n);
 
 
   }
